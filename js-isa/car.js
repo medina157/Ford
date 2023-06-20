c180ece372cfceba5198c0828965f84a4fe74d5a
medina157 <102966250+medina157@users.noreply.github.com>
2023-06-20 08:51:29 -0300

    Add files via upload

---
+++ js-isa/car.js
@@ -0,0 +1,53 @@
+
+function mouseEmCima(imagem) {
+   
+   imagem.src="Img-isa/img4.png"
+   
+   }
+   
+function mouseFora(imagem) {
+   
+   imagem.src="Img-isa/img3.png"
+   
+   }
+   
+   function mouseEmCima1(imagem) {
+   
+      imagem.src="Img-isa/zyro-image.png"
+      
+      }
+      
+   function mouseFora1(imagem) {
+      
+      imagem.src="Img-isa/img6.png"
+      
+      }
+     
+   
+      function mouseEmCima2(imagem) {
+      
+         imagem.src="Img-isa/img10 (2).png"
+                  
+                  }
+                  
+      function mouseFora2(imagem) {
+                  
+          imagem.src="Img-isa/img9 (2).png"
+                  
+                  }
+      function mouseEmCima3(imagem) {
+      
+         imagem.src="Img-isa/img11.png"
+                              
+          }
+                              
+      function mouseFora3(imagem) {
+                              
+          imagem.src="Img-isa/img12.png"
+                              
+                              }
+                              
+            
+                  
+
+
