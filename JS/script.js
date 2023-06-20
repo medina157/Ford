6e2bc3eb3c61c3f04459fdec0b50dbac117e5e07
medina157 <102966250+medina157@users.noreply.github.com>
2023-06-20 08:49:30 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,145 @@
+function img10(){
+    document.getElementById("trocar img").src="imgmedina/carro/Ranger_azul-removebg-preview.png";
+}
+function img11(){
+    document.getElementById("trocar img").src="imgmedina/carro/Ranger_vermelha-removebg-preview.png";
+}
+function img12(){
+    document.getElementById("trocar img").src="imgmedina/carro/Ranger_cinza-removebg-preview.png";
+}
+function img13(){
+    document.getElementById("trocar img").src="imgmedina/carro/Ranger_preta-removebg-preview.png";
+}
+function img14(){
+    document.getElementById("trocar img").src="imgmedina/carro/Ranger_branca-removebg-preview.png";
+}
+
+
+function img17(){
+    document.getElementById("trocar img").src="imgmedina/carro/maverick_azul-removebg-preview.png";
+}
+function img18(){
+    document.getElementById("trocar img").src="imgmedina/carro/maverick_branca-removebg-preview.png";
+}
+function img19(){
+    document.getElementById("trocar img").src="imgmedina/carro/maverick_cinza-removebg-preview.png";
+}
+function img20(){
+    document.getElementById("trocar img").src="imgmedina/carro/maverick_laranja-removebg-preview.png";
+}
+function img21(){
+    document.getElementById("trocar img").src="imgmedina/carro/maverick_preta-removebg-preview.png";
+}
+function img22(){
+    document.getElementById("trocar img").src="imgmedina/carro/maverick_vermelha-removebg-preview.png";
+}
+
+
+function img23(){
+    document.getElementById("trocar img").src="imgmedina/carro/fbranca.png";
+}
+function img24(){
+    document.getElementById("trocar img").src="imgmedina/carro/fpreta.png";
+}
+function img25(){
+    document.getElementById("trocar img").src="imgmedina/carro/fvermelha.png";
+}
+
+
+function img26(){
+    document.getElementById("trocar img").src="imgmedina/carro/bronco_azul-removebg-preview.png";
+}
+function img27(){
+    document.getElementById("trocar img").src="imgmedina/carro/bronco_branco-removebg-preview.png";
+}
+function img28(){
+    document.getElementById("trocar img").src="imgmedina/carro/bronco_cinza-removebg-preview.png";
+}
+function img29(){
+    document.getElementById("trocar img").src="imgmedina/carro/bronco_laranja-removebg-preview.png";
+}
+function img30(){
+    document.getElementById("trocar img").src="imgmedina/carro/bronco_marrom-removebg-preview.png";
+}
+function img31(){
+    document.getElementById("trocar img").src="imgmedina/carro/bronco_preto-removebg-preview.png";
+}
+function img32(){
+    document.getElementById("trocar img").src="imgmedina/carro/bronco_vermelho-removebg-preview.png";
+}
+
+
+
+function img33(){
+    document.getElementById("trocar img").src="imgmedina/carro/edazul.png";
+}
+function img34(){
+    document.getElementById("trocar img").src="imgmedina/carro/edbranca.png";
+}
+function img50(){
+    document.getElementById("trocar img").src="imgmedina/carro/edcinza.png";
+}
+function img51(){
+    document.getElementById("trocar img").src="imgmedina/carro/Edpreta.png";
+}
+function img52(){
+    document.getElementById("trocar img").src="imgmedina/carro/Edverde.png";
+}
+function img53(){
+    document.getElementById("trocar img").src="imgmedina/carro/Edvermelha.png";
+}
+function img54(){
+    document.getElementById("trocar img").src="imgmedina/carro/Edvinho.png";
+}
+
+function img49(){
+    document.getElementById("trocar img").src="imgmedina/carro/ecopreta-removebg-preview.png";
+}
+function img35(){
+    document.getElementById("trocar img").src="imgmedina/carro/ecobranca-removebg-preview.png";
+}
+function img45(){
+    document.getElementById("trocar img").src="imgmedina/carro/ecovermelha-removebg-preview.png";
+}
+function img46(){
+    document.getElementById("trocar img").src="imgmedina/carro/ecocinza-removebg-preview.png";
+}
+function img47(){
+    document.getElementById("trocar img").src="imgmedina/carro/ecoazul-removebg-preview.png";
+}
+function img48(){
+    document.getElementById("trocar img").src="imgmedina/carro/ecoamarela-removebg-preview.png";
+}
+
+
+function img37(){
+    document.getElementById("trocar img").src="imgmedina/carro/territory_branco-removebg-preview.png";
+}
+function img38(){
+    document.getElementById("trocar img").src="imgmedina/carro/territory_vermelho-removebg-preview.png";
+}
+function img39(){
+    document.getElementById("trocar img").src="imgmedina/carro/territory_preto-removebg-preview.png";
+}
+function img40(){
+    document.getElementById("trocar img").src="imgmedina/carro/territory_marrom-removebg-preview.png";
+}
+
+
+function img41(){
+    document.getElementById("trocar img").src="imgmedina/carro/minivan_branca-removebg-preview.png";
+}
+function img42(){
+    document.getElementById("trocar img").src="imgmedina/carro/minivan_cinza-removebg-preview.png";
+}
+
+
+function img43(){
+    document.getElementById("trocar img").src="imgmedina/carro/furgao_branco-removebg-preview.png";
+}
+function img44(){
+    document.getElementById("trocar img").src="imgmedina/carro/furgao_cinza-removebg-preview.png";
+}
+
+
+
